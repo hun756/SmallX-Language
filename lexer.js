bf53e951a09bd5b66194b5c4f90187fe6508114f
Mehmet Şükrü Ekemen <56188619+hun756@users.noreply.github.com>
2022-03-09 19:18:22 +0300

    Add Parsing Context
      -  Add identifier
      -  Add braces
      -  Add arrow and assignment operator

---
+++ lexer.js
@@ -0,0 +1,38 @@
+const moo = require('moo')
+const fs = require('mz/fs');
+
+let lexer =
+    moo.compile({
+        WS: /[ \t]+/,
+        comment: /\/\/.*?$/,
+        number: /0|[1-9][0-9]*/,
+        string: /"(?:\\["\\]|[^\n"\\])*"/,
+        lparen: '(',
+        rparen: ')',
+        lbrace: '{',
+        rbrace: '}',
+        identifier: /[a-zA-Z][a-zA-Z_0-9]*/,
+        fatarrow: '=>',
+        assign: '=',
+        NL: { match: /\n/, lineBreaks: true },
+    }
+);
+
+// lexer.reset(`apple a9913`);
+// lexer.reset(`=> =`);
+
+async function main() {
+    const code = (await fs.readFile("langfiles/prog1.smlx")).toString();
+    lexer.reset(code);
+    while (true) {
+        const token = lexer.next();
+        if (!token) {
+            break;
+        }
+        console.log(token);
+    }
+}
+
+console.log("deneem");
+
+main().catch(err => console.log(err.stack));
